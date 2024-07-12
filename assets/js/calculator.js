7ee8c33da96766fdf3ec5b5a754ff29eeed1759d
Frank <kamunyuz@gmail.com>
2024-07-12 17:34:24 +0300

    implented a cross mark on bmi-output

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -12,7 +12,7 @@ function calculateBMI() {
   let outputMessage = '';
 
   if (isNaN(bmi)) {
-      outputMessage = 'Please enter valid height and weight values!';
+      outputMessage = 'Please enter valid height and weight values ❌';
   } else if (bmi < 18.5) {
       outputMessage = `Your BMI is ${bmi.toFixed(2)} and you are underweight 😟`;
   } else if (bmi >= 18.5 && bmi < 24.9) {
